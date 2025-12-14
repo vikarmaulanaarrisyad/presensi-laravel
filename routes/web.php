d6910a10e7102858b7b851962b9c9a4c186cee66
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2025-12-14 15:25:02 +0700

    add template frontend

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('auth.user.login');
+});
+Route::get('/dashboard', function () {
     return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+})->name('dashboard');
