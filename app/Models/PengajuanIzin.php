8c582ac856f8ca824808ea17e3eec6f916d0b682
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2026-01-09 06:12:40 +0700

    add fitur approved dan batal approved

--- app/Models/PengajuanIzin.php
+++ app/Models/PengajuanIzin.php
@@ -2,8 +2,34 @@
 
 namespace App\Models;
 
-
 class PengajuanIzin extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'disetujui_oleh');
+    }
+
+    // =====================
+    // SCOPE (OPSIONAL)
+    // =====================
+
+    public function scopeMenunggu($query)
+    {
+        return $query->where('status', 'menunggu');
+    }
+
+    public function scopeDisetujui($query)
+    {
+        return $query->where('status', 'disetujui');
+    }
+
+    public function scopeDitolak($query)
+    {
+        return $query->where('status', 'ditolak');
+    }
 }
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -140,7 +140,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="{{ route('persetujuan.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Izin / Sakit</p>
                             </a>
